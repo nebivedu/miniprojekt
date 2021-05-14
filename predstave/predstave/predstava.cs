8bf47d0876c1ac324eb2ba9fc01b690369bcafa1
Gašper Cilenšek <gapiga71@gmail.com>
2021-05-14 16:44:34 +0200

    Class predstave

---
+++ predstave/predstave/predstava.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace predstave
+{
+    class predstava
+    {
+        public double Id { get; set; }
+        public string Ime { get; set; }
+        public string Zvrst { get; set; }
+        public string Datum { get; set; }
+        public string Opis { get; set; }
+        public string Lokacija { get; set; }
+        
+
+
+        public predstava()
+        {
+        }
+        public predstava(int id)
+        {
+            Id = id;
+
+        }
+        public predstava(int id, string ime, string zvrst, string datum, string opis, string lokacija)
+        {
+
+            Id = id;
+            Ime = ime;
+            Zvrst = zvrst;
+            Datum = datum;
+            Opis = opis;
+            Lokacija = lokacija;
+            
+
+        }
+        
+        public override string ToString()
+        {
+            string vstring = Id + Ime + Zvrst + Datum + Opis + Lokacija;
+
+            return vstring;
+        }
+    }
+}
